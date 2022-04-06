8ad9f0c104d7e918b48980000f57d49cfbbdc610
crossliner <crosslinerdev@gmail.com>
2022-04-06 12:03:44 +0300

    feat(project): finished

--- mods/server/mod.ts
+++ mods/server/mod.ts
@@ -1,33 +1,55 @@
 import { serve } from "std/http/server.ts";
-import { inProduction, json, isVaildApiKey } from "@content-serve/utils";
+import { 
+    inProduction, 
+    json, 
+    isVaildApiKey,
+    generateFileName,
+    getUrl 
+} from "@content-serve/utils";
 
+import { mime } from "mime-types";
+import { join } from "std/path/posix.ts";
 
 serve(async (req) => {
     const type = req.headers.get("content-type") || ""
     const url = new URL(req.url);
     
     if (url.pathname === "/upload" && req.method === "POST" && type.startsWith("multipart/form-data")) {
-        if (isVaildApiKey(req.headers.get("x-upload-key"))) return json({
+        if (!isVaildApiKey(req.headers.get("x-upload-key"))) return json({
             success: false,
             error: "unauthorized to use the endpoint"
         }, 401);
         
         const data = await req.formData();
         const file = <File>data.get("file");
-        if (!file) return json({
+        if (!file || !(file instanceof File)) return json({
             success: false,
             error: "invaild form configuration"
         }, 400);
-        
-        // const buf = await file.arrayBuffer();
+
+        const buf = await file.arrayBuffer();
+        const name = generateFileName(file.name);
+        await Deno.writeFile(join("uploads", name), new Uint8Array(buf));
         
         return json({ 
-            success: true
+            success: true,
+            url: getUrl(name)
         }, 201);
     }
     
-    return json({ 
-        success: false,
-        error: "resource not found"
-    }, 404)
+    try {
+        const file = await Deno.readFile(join("uploads", url.pathname));
+        const type = mime.getType(url.pathname);
+        return new Response(file.buffer, {
+            headers: {
+                "Content-Type": type!
+            }
+        });
+    } catch(err) {
+        return json({ 
+            success: false,
+            error: "resource not found"
+        }, 404)
+    }
+    
 }, { port: inProduction() ? 80 : 4500 });
--- mods/utils/mod.ts
+++ mods/utils/mod.ts
@@ -1,5 +1,8 @@
+import { cryptoRandomString } from "randomstring";
+import { extname } from "std/path/posix.ts";
+
 export function inProduction() {
-    return Deno.env.get("DENO_ENV") === "production" ? true : false;
+    return Deno.env.get("DENO_ENV") === "production";
 };
 
 export function json(data: any, status: number = 200) {
@@ -17,4 +20,14 @@ export function json(data: any, status: number = 200) {
 export function isVaildApiKey(key: string | null) {
     const apiKey = inProduction() ? Deno.env.get("API_KEY") : "test";
     return apiKey === key;
+}
+
+export function generateFileName(name: string) {
+    const ext = extname(name);
+    return cryptoRandomString({ length: 8, type: "alphanumeric" }) + ext;
+}
+
+export function getUrl(name: string) {
+    const url = inProduction() ? Deno.env.get("URL") : "http://localhost:4500/";
+    return url + name;
 }
